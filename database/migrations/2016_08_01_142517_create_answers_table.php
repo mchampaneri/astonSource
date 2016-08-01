0cd734bf90519fba39ba1be8a3ea4a71621ebed5
Manish <m.champaneri.20@gmail.com>
2016-08-01 20:51:13 +0530

    Initialing the project

---
+++ database/migrations/2016_08_01_142517_create_answers_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAnswersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('answers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('answers');
+    }
+}
